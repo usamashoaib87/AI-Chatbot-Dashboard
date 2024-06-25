e218bee9d810227b73bff2368f254bdfa43edf85
usamashoaib87 <usamashoaib87@gmail.com>
2024-06-25 16:19:56 +0500

    settings tb

--- src/Components/SidebarPages/Settings/Tabs/AccountPreferences/AccountPreferences.jsx
+++ src/Components/SidebarPages/Settings/Tabs/AccountPreferences/AccountPreferences.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import '../../Settings.scss'
+import React from "react";
+import "../../Settings.scss";
 
 const AccountPreferences = () => {
   return (
-    <div>
-      <h2>Account Preferences</h2>
-      
+    <div className="acc-pref">
+      <div>
+        <h3>Change password</h3>
+      </div>
+      <div className="pass-detail">
+        <p>Change password</p>
+        <input type="password" placeholder="  * * * * * * * *"></input>
+        <div className="retype">
+          <div>
+            <p>New password</p>
+            <input type="password" placeholder="  * * * * * * * *"></input>
+          </div>
+          <div>
+            <p>Retype new password</p>
+            <input type="password" placeholder="  * * * * * * * *"></input>
+          </div>
+        </div>
+        <button>Update password</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AccountPreferences
+export default AccountPreferences;
--- src/Components/SidebarPages/Settings/Tabs/ApiKeys/ApiKeys.jsx
+++ src/Components/SidebarPages/Settings/Tabs/ApiKeys/ApiKeys.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import '../../Settings.scss'
+import React from "react";
+import "../../Settings.scss";
 const ApiKeys = () => {
   return (
-    <div>
-       <h2>API Keys</h2>
-       <p>Content for API Keys</p>
+    <div className="api-keys">
+      <div>
+        <p>Add your own OpenAI key </p>
+      </div>
+      <div className="inner-input">
+        <div>
+          <label>OpenAI key</label> <br />
+          <input type="text" placeholder="  Key"></input>
+        </div>
+        <div>
+          <label>OpenAI key</label> <br />
+          <select>
+            <option>Select a GPT modal</option>
+            <option>GPT-4</option>
+            <option>GPT-5</option>
+            <option>GPT-6</option>
+          </select>
+        </div>
+      </div>
+      <div>
+        <button>Save key</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ApiKeys
+export default ApiKeys;
