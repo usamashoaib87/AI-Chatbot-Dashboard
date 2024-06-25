46fb689cc3169f46d52d5df196a8a53215b8b277
usamashoaib87 <usamashoaib87@gmail.com>
2024-06-25 12:56:10 +0500

    settings tb

--- src/Components/SidebarPages/Monitor/Monitor.jsx
+++ src/Components/SidebarPages/Monitor/Monitor.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Monitor.scss';
 import {Bar} from 'react-chartjs-2';
-// import Chart from 'chart.js/auto';
+import Chart from 'chart.js/auto';
 
 const Monitor = () => {
   return (
