7567903b44218a134fcc4e527cc70c24b88524bf
usamashoaib87 <usamashoaib87@gmail.com>
2024-06-25 12:41:35 +0500

    settings tb

--- src/Components/SidebarPages/Monitor/Monitor.jsx
+++ src/Components/SidebarPages/Monitor/Monitor.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Monitor.scss';
 import {Bar} from 'react-chartjs-2';
-import Chart from 'chart.js/auto';
+// import Chart from 'chart.js/auto';
 
 const Monitor = () => {
   return (
@@ -61,7 +61,8 @@ const Monitor = () => {
                 },
               ],
               options :{
-                maintainAspectRatio: false
+                maintainAspectRatio: false,
+                
               },
               
             }}
